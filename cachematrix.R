9a897f98d433a3b376dcf11dcb524981f9c2c95c
Hugh Beever <hugh.beever@gmail.com>
2015-10-25 21:50:58 +0000

    Revised variable names, removed superfluous code

--- cachematrix.R
+++ cachematrix.R
@@ -4,36 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(myMatrix = matrix()) {
-
-    inverse <- NULL
-    if (det(myMatrix) == 0) {
-      message("Supplied matrix is not invertible")
+  
+  inverse <- NULL
+  if (det(myMatrix) == 0) {
+    message("Supplied matrix is not invertible")
+  }
+  else {
+    set <- function(y) {
+      myMatrix <<- y
+      inverse <<- NULL
     }
-    else {
-      set <- function(y) {
-        myMatrix <<- y
-        inverse <<- NULL
-      }
-      get <- function() myMatrix
-      setinverse <- function(invMatrix) inverse <<- invMatrix
-      getinverse <- function() inverse
-      list(set = set, get = get,
-           setinverse = setinverse,
-           getinverse = getinverse)
-    }
+    get <- function() myMatrix
+    setinverse <- function(invMatrix) inverse <<- invMatrix
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  }
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'inMatrix'
-  i <- x$getinverse()
+cacheSolve <- function(inList) {
+  ## Return a matrix that is the inverse of 'inMatrix'
+  i <- inList$getinverse()
   if(!is.null(i)) {
     message("getting cached data")
   }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
+  data <- inList$get()
+  i <- solve(data)
+  inList$setinverse(i)
   return(i)
-  }
+}
