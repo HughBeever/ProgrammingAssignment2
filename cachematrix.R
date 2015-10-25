b0e3ddc747c60bfd0ef63ca67a364f81db4f4676
Hugh Beever <hugh.beever@gmail.com>
2015-10-25 21:50:13 +0000

    Code now working, needs tidying

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(myMatrix = matrix()) {
 
+    inverse <- NULL
+    if (det(myMatrix) == 0) {
+      message("Supplied matrix is not invertible")
+    }
+    else {
+      set <- function(y) {
+        myMatrix <<- y
+        inverse <<- NULL
+      }
+      get <- function() myMatrix
+      setinverse <- function(invMatrix) inverse <<- invMatrix
+      getinverse <- function() inverse
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+    }
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        ## Return a matrix that is the inverse of 'inMatrix'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  return(i)
+  }
